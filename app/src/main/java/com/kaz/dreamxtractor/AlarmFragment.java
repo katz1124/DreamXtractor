36ce9eaac05dbbf7b83740cef49286c25598c596
katz1124 <60231010+katz1124@users.noreply.github.com>
2021-10-31 15:22:15 -0600

    avance sin q se func en la app

--- app/src/main/java/com/kaz/dreamxtractor/AlarmFragment.java
+++ app/src/main/java/com/kaz/dreamxtractor/AlarmFragment.java
@@ -2,23 +2,32 @@ package com.kaz.dreamxtractor;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link AlarmFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AlarmFragment extends Fragment {
+public class AlarmFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private NavController navController;
+    private ImageView listBtn;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -52,7 +61,17 @@ public class AlarmFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController= Navigation.findNavController(view);
+        listBtn= view.findViewById(R.id.record_list_btn);
+        listBtn.setOnClickListener(this);
     }
 
     @Override
@@ -60,5 +79,14 @@ public class AlarmFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_alarm, container, false);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.record_list_btn:
+                navController.navigate(R.id.action_alarmFragment_to_audioListFragment);
+                break;
+        }
     }
 }
